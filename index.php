e5156b5a3d0c490f509cc9e86817a602d26aa977
Joseph Asare <asare.joseph33@gmail.com>
2020-01-16 12:01:51 +0000

    index page updated with a nav bar.

--- index.php
+++ index.php
@@ -11,11 +11,26 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<title>Checkout</title>
+	<title>Home</title>
 	
 </head>
 <body>
 	<div class="container px-lg-5 mt-4">
+	<div>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="index.php">iZZY-Sell</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
+				<a class="nav-item nav-link" href="customers.view.php">Customers</a>
+				<a class="nav-item nav-link" href="#">About Us</a>
+			</div>
+		</div>
+		</nav>
+	</div>
+	<br>
+	<hr>
 	<!-- first card-->
 	<div class="card" style="width: 18rem;">
 		<img src="/img/apple-iphone-x.jpg" class="card-img-top" alt="...">
