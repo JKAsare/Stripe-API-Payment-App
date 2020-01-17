1f24bea80851d006db824ad39ea2a310c2079025
Joseph Asare <asare.joseph33@gmail.com>
2020-01-17 15:58:09 +0000

    deleted three images in img.

--- config/db.php
+++ config/db.php
@@ -4,5 +4,5 @@
 
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "@Accra19");
-define("DB_NAME", "paypage");
+define("DB_PASS", " "); // if you have password for your database, put it here else leave it as it is.
+define("DB_NAME", "paypage"); // create a database named: paypage or any name you like but make you enter is here.
--- index.php
+++ index.php
@@ -17,6 +17,7 @@
 <body>
 	<div class="container px-lg-5 mt-4">
 	<div>
+	<!-- navbar -->
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<a class="navbar-brand" href="index.php">iZZY-Sell</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
@@ -29,6 +30,7 @@
 		</div>
 		</nav>
 	</div>
+	<!-- navbar ends here --> 
 	<br>
 	<hr>
 	<!-- first card-->
